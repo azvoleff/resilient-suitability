6e86310ef41650f8d3a117458fcce488dcb7196f
Alex Zvoleff <azvoleff@gmail.com>
2016-06-28 16:57:34 -0400

    Fix filenames.

--- land_saving.R
+++ land_saving.R
@@ -1,52 +1,33 @@
-# Identify possible areas for intensification
+# Preserve lands that provide key ecosystem services
 #
-# Considers projected changes in agro-climatic suitability and current (year 
-# 2000) yield gaps.
-possible_maize_areas <- function(iso3='TZA') {
-    ##########################################################################
-    ### Find areas where yields should increase or stay stable with clim chg
-
-    # Load current and future agro-climatic yield
-    # TODO: need to use median of all climate model outputs
-    
-    # This is agroc-climatically attainable yield for intermediate input level 
-    # rain-fed maize for future period 2050s from Hadley CM3 B2 scenario
-    acy_fut <- raster(file.path(data_base, 'HarvestChoice', 'res02_h3a22020i_maiz150b_yld.tif'))
-    acy_fut <- setup_raster_layer(acy_fut)
-    acy_cur <- raster(file.path(data_base, 'HarvestChoice', 'res02_crav6190i_maiz150b_yld.tif'))
-    acy_cur <- setup_raster_layer(acy_cur)
-
-    # Calculate difference in agro-climatic yield with climate change, and mask 
-    # out areas that will see a decline in agro-climatic yield
-    acy_exclude <- (acy_fut - acy_cur) / acy_cur >= -.05
-
-    acy_exclude_poly <- rasterToPolygons(acy_exclude)
-    writeOGR(acy_exclude_poly, 'AGRA_TZA_acy_exclude.kml', 'acy_exclude', 
-             driver='KML', overwrite=TRUE)
-
-    ##########################################################################
-    ### Find areas where large yield gap exists
-
-    # Ratio of actual and potential yield for rain-fed maize
-    ygap_ratio <- raster(file.path(data_base, 'HarvestChoice', 'gap2000_r_mze_2000_yga_cl.tif'))
-    ygap_ratio <- setup_raster_layer(ygap_ratio)
-
-    # Difference of potential and actual production for rain-fed maize
-    ygap_diff <- raster(file.path(data_base, 'HarvestChoice', 'gap2000_r_mze_2000_qga.tif'))
-    ygap_diff <- setup_raster_layer(ygap_diff)
-
-    ygap_diff_poly <- rasterToPolygons(ygap_diff)
-    writeOGR(ygap_diff_poly, 'AGRA_TZA_ygap_diff.kml', 'ygap_diff', 
-             driver='KML', overwrite=TRUE)
-
-    # Normalize by largest gap
-    ygap_diff_norm <- ygap_diff / cellStats(ygap_diff, 'max')
-
-    return(ygap_diff * acy_exclude)
+# Exclude areas from consideration based on WDPA, Forest Cover (Hansen et al.  
+# 2013) and freshwater availability from WaterWorld.
+get_forest_areas <- function(iso3='TZA') {
+    output_folder <- 'H:/Data/GFC_Product'
+    aoi <- get_country_poly()
+    aoi <- gUnaryUnion(aoi)
+    tiles <- calc_gfc_tiles(get_country_poly())
+    download_tiles(tiles, output_folder)
+    gfc_extract <- extract_gfc(aoi, output_folder)
 }
 
-poss <- possible_maize_areas()
-poss_poly <- rasterToPolygons(poss)
-writeOGR(poss_poly, 'AGRA_TZA_poss_areas.kml', 'AGRA_TZA_poss_areas', 
-         driver='KML', overwrite=TRUE)
+excluded_areas <- function(iso3='TZA') {
+    forests <- get_forest_areas()
+    
+    ##########################################################################
+    ### Protected areas
+    dataset <- "H:/Data/WDPA/WDPA_June2016/WDPA_June2016-shapefile-polygons.shp"
+    wdpa <- setup_vector_layer(dataset)
+    wdpa <- gUnaryUnion(wdpa)
+    wdpa_ok <- rasterize(wdpa, base, 0, background=1)
+
+    wdpa_ok_poly <- rasterToPolygons(wdpa_ok)
+    writeOGR(wdpa_ok_poly, 'AGRA_TZA_wdpa_ok.kml', 'wdpa_ok', driver='KML', 
+             overwrite=TRUE)
+
+    ##########################################################################
+    ### Water
+}
+
+poss <- poss * wdpa_ok
 
